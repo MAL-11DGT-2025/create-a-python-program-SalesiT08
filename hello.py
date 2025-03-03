dfc1a6d298a707804cf637b64c61ef87fca30937
SalesiT08 <salesi.talakai@marcellin.school.nz>
2025-03-04 10:55:04 +1300

    i wrote more code

--- hello.py
+++ hello.py
@@ -4,5 +4,10 @@ print("My favourite colour is blue")
 name = input("what is your name: ")
 print(f"Kia ora {name}")
 
-age = input("what is your age: ")
+age = int(input("what is your age: "))
 print(f"{name}, did you say you were {age}? you are old.")
+
+
+if age > 25:
+    print("you are old")
+        
