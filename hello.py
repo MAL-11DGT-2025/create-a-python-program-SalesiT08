032e6b4ec90c237c456e33101d8caf6d9ff8b2f3
SalesiT08 <salesi.talakai@marcellin.school.nz>
2025-03-04 10:44:26 +1300

    I wrote more code

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
 print("Hello, World!")
+print("My favourite colour is blue")
+
+name = input("what is your name: ")
+print(f"Kia ora {name}")
+
+age = input("what is your age: ")
+print(f"{name}, did you say you were {age}? you are old.")
